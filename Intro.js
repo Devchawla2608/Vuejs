4004df08cbc26c847e4bf99c1bfb47a72500ae74
Deepanshu Chawla <dev.chawla2608@gmail.com>
2023-12-09 17:30:08 +0530

    Intro

---
+++ Intro.js
@@ -0,0 +1,22 @@
+Vue js is a javascript framework for building application/
+
+How Vue Helps ?
+1.) Fast Application
+2.) Dynamic Application 
+3.) Speed of development 
+4.) Developer Tools 
+5.) Helps with scaling 
+
+// ! Vue Js
+// Virtual dom framework 
+// Lightweight(10k gzip)
+// Progressive 
+// Flexible 
+// Incrementally adoptable 
+
+// History of Vue.js
+
+
+// Installation
+// CDN - @next is important becuase v3 is not in main repo of vue js . It is in other repo
+<script src="https://unpkg.com/vue@next"></script>
