27adf88dc8ca806b3585615e86974b28090880b1
Deepanshu Chawla <dev.chawla2608@gmail.com>
2023-12-10 19:08:18 +0530

    Vue With Cdn

--- Intro.js
+++ Intro.js
@@ -1,11 +1,11 @@
-Vue js is a javascript framework for building application/
+// Vue js is a javascript framework for building application/
 
-How Vue Helps ?
-1.) Fast Application
-2.) Dynamic Application 
-3.) Speed of development 
-4.) Developer Tools 
-5.) Helps with scaling 
+// How Vue Helps ?
+// 1.) Fast Application
+// 2.) Dynamic Application 
+// 3.) Speed of development 
+// 4.) Developer Tools 
+// 5.) Helps with scaling 
 
 // ! Vue Js
 // Virtual dom framework 
@@ -29,3 +29,63 @@ How Vue Helps ?
 // @click.right - right click 
 // @click.prevent - prevent = event.preventDefault
 // @click.prevent.stop 
+
+// Components
+// Till now we just worked on single html file .
+// Now we will just break our application into several parts to make larger applications 
+
+
+
+
+
+//! What is Vue js
+// 1.) Javascript Framework for building web app 
+// 2.) Vue js is progressive Framework
+// 3.) Vue js 3 is the current version
+// 4.) We make single page application in Vue js 
+
+//! History of Vue js
+// Initial Release Feb 2014 
+// The current version is 3.0.5 (Major , Minor , Bug)
+// Vue is created by Evan You 
+// Evan you were working for google
+
+
+//! Main Topic of series 
+// Intro 
+// setup 
+// How to start 
+// Components 
+// Conditions and Loops 
+// Forms  
+// Api 
+// Life cycle 
+// Routing  
+// State Management 
+// Small Project 
+// Build 
+// 3rd Party Library 
+// Interview Questions 
+
+// ! • How to use Vue js without Node and
+// • Make Index.html file
+// • Make app.js file
+// • Add Vue js cdn Link
+// A • Write A code with vue js
+
+
+// ! Why we include app js script file in the last of html file 
+// because we want to mount our vuejs application in component with id app  but till then app is not created if we put script in head
+
+// ! Can we use Vue js with cdn
+// Yes 
+
+// ! Is is write way to use cdn instead of npm
+// No becuase after building it does not comparess that much
+
+// ! How to Install Vue js
+// • Install Node and NPM
+// • Install Vue Cli
+// • Create Application
+// • Run Application
+// • Check Output
---
+++ app.js
@@ -0,0 +1,4 @@
+const app = Vue.createApp({
+    template:"<h1>Vue Js Tutorial</h1>"
+})
+app.mount('#app')
