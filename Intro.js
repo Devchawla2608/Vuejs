db6b92015289b356b9b0117b76dadc4243f133cb
Deepanshu Chawla <dev.chawla2608@gmail.com>
2024-01-21 13:24:40 +0530

    Vue js With CDN

--- Intro.js
+++ Intro.js
@@ -1,50 +1,6 @@
-// ! Vue js is a javascript framework for building application/
-
-// ! How Vue Helps ?
-// 1.) Fast Application
-// 2.) Dynamic Application 
-// 3.) Speed of development 
-// 4.) Developer Tools 
-// 5.) Helps with scaling 
-
-// ! Vue Js
-// 1.) Virtual dom framework 
-// 2.) Lightweight(10k gzip)
-// 3.) Progressive 
-// 4.) Flexible 
-// 5.) Incrementally adoptable 
-
-// ! what is Vue js ?
-// 1.) Javascript Framework for building web app 
-// 2.) Vue js is progressive framework
-// 3.) Vue js 3 is the current version 
-// 4.) We make SPA in vue js
-
-
-// ! History of Vue.js
-// 1.) Initial Release 
-// 2.) Current version is 3.0.5
-// 3.) Vuejs 3 is the current version 
-// 4.) Created by Evan You 
-// 5.) Evan you were working for google 
-
-
-
-// ! Main Topic of this Series
-// • Introduction
-// • Setup and Installation
-// • Life Cycle.
-// • Routing
-// • State Managment
-// • How to start with code
-// • Component
-// • Condition and Loops
-// • Form handing
-// • API integration
-// • Small Project
-// • BuildI
-// • 3rd Party Library
-// • Interview Questions
-
-
-
+// ! Vue without Installation
+// 1.) How to use Vue js without Node and NPM 
+// 2.) Make indexedDB.html file
+// 3.) Make app.js file 
+// 4.) Add Vue js CDN link 
+// 5.) Write A code with Vue js 
---
+++ app.js
@@ -0,0 +1,5 @@
+console.log("Hello World!")
+let app = Vue.createApp({
+    template: `<h1>Hello Vue js</h1>`
+});
+app.mount('#app') 
